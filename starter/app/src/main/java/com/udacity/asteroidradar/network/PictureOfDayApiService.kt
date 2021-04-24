ddadd6efb0a0a4c3f9b2c33f3fa123c529f04003
Elias Mazzocco <elias.mazzocco@mytaxi.com>
2021-04-24 09:58:17 +0200

    Create the picture of the day service

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/network/PictureOfDayApiService.kt
@@ -0,0 +1,9 @@
+package com.udacity.asteroidradar.network
+
+import com.udacity.asteroidradar.common.Constants
+import retrofit2.http.GET
+
+interface PictureOfDayApiService {
+    @GET("/planetary/apod?api_key=" + Constants.API_KEY)
+    suspend fun getPictureOfDay(): PictureOfDay
+}
