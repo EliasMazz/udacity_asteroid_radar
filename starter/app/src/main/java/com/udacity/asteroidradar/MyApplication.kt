0b517fa7fea7827be2ad693e1f06bbbece29226c
Elias Mazzocco <elias.mazzocco@mytaxi.com>
2021-04-24 13:44:56 +0200

    Initialize Timber

--- starter/app/src/main/java/com/udacity/asteroidradar/MyApplication.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/MyApplication.kt
@@ -2,12 +2,20 @@ package com.udacity.asteroidradar
 
 import android.app.Application
 import com.udacity.asteroidradar.common.dependencyinjection.CompositionRoot
+import timber.log.Timber
 
 class MyApplication : Application() {
 
-    val applicationCompositionRoot = CompositionRoot()
+    val applicationCompositionRoot by lazy { CompositionRoot(this) }
 
     override fun onCreate() {
         super.onCreate()
+        initTimber()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
